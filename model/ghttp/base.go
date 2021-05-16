922e320cd6d807beeb1cc5e900ee1dbd601f2e73
liziwei01 <alssylk@gmail.com>
2021-05-16 18:02:10 +0800

    Update base.go

--- model/ghttp/base.go
+++ model/ghttp/base.go
@@ -6,22 +6,17 @@ import (
 	errBase "github.com/liziwei01/go-liziwei01-library/model/error"
 )
 
-type (
-	Request  *http.Request
-	Response http.ResponseWriter
-)
-
 type Ghttp interface {
-	Request() *Request
-	Response() *Response
+	Request() **http.Request
+	Response() *http.ResponseWriter
 }
 
 type ghttp struct {
-	request  *Request
-	response *Response
+	request  **http.Request
+	response *http.ResponseWriter
 }
 
-func Default(request *Request, response *Response) Ghttp {
+func Default(request **http.Request, response *http.ResponseWriter) Ghttp {
 	(*response).Header().Set("content-type", "text/json")
 	(*response).Header().Set("Access-Control-Allow-Origin", "*")
 	return &ghttp{
@@ -30,12 +25,26 @@ func Default(request *Request, response *Response) Ghttp {
 	}
 }
 
-func (g *ghttp) Request() *Request {
+func (g *ghttp) Request() **http.Request {
 	return g.request
 }
 
-func (g *ghttp) Response() *Response {
+func (g *ghttp) Response() *http.ResponseWriter {
 	return g.response
+}
+
+func Get(g Ghttp, str string) string {
+	return (**g.Request()).URL.Query().Get(str)
+}
+
+func Post(g Ghttp, str string) string {
+	(**g.Request()).ParseForm()
+	for k, v := range (**g.Request()).PostForm {
+		if k == str {
+			return v[0]
+		}
+	}
+	return ""
 }
 
 func Write(g Ghttp, data interface{}, errno int, err error) {
