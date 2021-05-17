f3938962be5e3673efdc7a4732646ad96ced6904
liziwei01 <alssylk@gmail.com>
2021-05-17 23:04:24 +0800

    improve log

--- model/log/base.go
+++ model/log/base.go
@@ -4,30 +4,14 @@ import (
 	lib "github.com/baidu/go-lib/log"
 )
 
-// type LogFunc func(arg0 interface{}, args ...interface{})
-
-// var (
-// 	InfoFunc  LogFunc = lib.Logger.Info
-// 	WarnFunc  LogFunc = lib.Logger.Warn
-// 	ErrorFunc LogFunc = lib.Logger.Error
-// )
-
 func Init(programName string) error {
+	return setLog(programName)
+}
+
+func setLog(programName string) error {
 	err := lib.Init(programName, "INFO", "./log", true, "H", 5)
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func Info() func(arg0 interface{}, args ...interface{}) {
-	return lib.Logger.Info
-}
-
-func Warn() func(arg0 interface{}, args ...interface{}) error {
-	return lib.Logger.Warn
-}
-
-func Error() func(arg0 interface{}, args ...interface{}) error {
-	return lib.Logger.Error
 }
